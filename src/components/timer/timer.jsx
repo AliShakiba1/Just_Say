7204cd4137599903ee5252b84018083978e81bbd
AliShakiba <prance5731.mania@gmail.com>
2023-05-11 11:08:10 +0330

    fix under 10 number ( pad start )

--- src/components/timer/timer.jsx
+++ src/components/timer/timer.jsx
@@ -11,11 +11,14 @@ const timer = () => {
 
   const getTime = () => {
     const time = Date.parse(deadLine) - Date.now()
-    setDays(Math.floor(time / (1000 * 60 * 60 * 24)))
-    setHours(Math.floor((time / (1000 * 60 * 60)) % 24))
-    setMinutes(Math.floor((time / 1000 / 60) % 60))
-    setSeconds(Math.floor((time / 1000) % 60))
+    
+    setDays(convertPadStart(Math.floor(time / (1000 * 60 * 60 * 24))))
+    setHours(convertPadStart(Math.floor((time / (1000 * 60 * 60)) % 24)))
+    setMinutes(convertPadStart(Math.floor((time / 1000 / 60) % 60)))
+    setSeconds(convertPadStart(Math.floor((time / 1000) % 60)))
   }
+
+  const convertPadStart = n => (n > 9 ? '' + n : '0' + n)
 
   useEffect(() => {
     const interval = setInterval(() => getTime(), 1000)
